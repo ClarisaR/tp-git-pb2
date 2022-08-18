9a195fe6e152a0b92d30006fc8e625e26a2c3bc8
ClarisaR <98982265+ClarisaR@users.noreply.github.com>
2022-08-18 16:07:35 -0300

    Agregue syso

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -10,7 +10,7 @@ public class Calculadora {
 
 		do {
 			System.out.println(
-					"Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+					"Ingrese su operaciÃ³n /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
 			opcion = teclado.nextInt();
 		} while (opcion < 1 && opcion > 4);
 		System.out.println("Ingrese el primer numero");
@@ -29,5 +29,6 @@ public class Calculadora {
 		System.out.println("El resultado es " + resultado.toString());
 		
 		teclado.close();
+		System.out.println("Prueba");
 	}
 }
